673cd96168878da4d4055f2390f74dd2f5a1f1c2
rickeymandraque <rickeymandraque@users.noreply.github.com>
2021-05-20 14:19:47 +0200

    Create isitdown.sh
    simple script d'exemple avec curl pour récupérer les infos et les mettre dans un cadre.

---
+++ isitdown.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# coloration du texte
+RED='\033[38;5;160m' #ex echo -e "${RED} ALERT"
+NC='\033[0m'    #ex echo -e "${NC} Normal"
+GREEN='\033[38;1;32m'   #ex echo -e "${GREEN} OK"
+YELLOW='\033[38;5;226m' #ex echo -e "${YELLOW} Warning"
+
+#fonction d'aide standard pour illustration
+function show_usage() {
+	printf "Usage: $0 \"url\" \n"
+	printf "\n"
+	printf " Options:\n\n"
+	printf "  -h | --help \t\t Voir l'aide\n\n"
+
+	return 0
+}
+if [[ $1 == "--help"   ]] || [[ $1 == "-h"   ]]; then
+	show_usage
+elif  [ -z "$1" ]; then
+	printf ""${RED}"Erreur"${NC}" url manquante\n"
+	show_usage
+else
+
+	# mise en boite
+	# je ne comprend pas cette partie et je ne sais pas comment l'adaptée
+	function box_out() {
+		local s=("$@") b w
+		for l in "${s[@]}"; do
+			((w < ${#l})) && {
+				b="$l"
+				w="${#l}"
+			}
+		done
+		tput setaf 3
+		echo -e "\t=${b//?/=}=
+\t|| ${b//?/ } \t||"
+		for l in "${s[@]}"; do
+			printf '\t||\t %s%*s%s \t||\n' "$(tput setaf 4)" "-$w" "$l" "$(tput setaf 3)"
+		done
+		echo -e "\t|| ${b//?/ } \t||
+ \t=${b//?/=}="
+		tput sgr 0
+	}
+	# script qui ne sert pas à grand chose, juste pour illustration
+
+	if curl --output /dev/null --silent --head --fail "$1"; then
+		URL_EXIST="L'url est joingnable"
+		IP_HOSTNAME="$(dig +short "$1")"
+		Color="${GREEN}"
+	else
+		URL_EXIST="L'url est injoignable"
+		IP_HOSTNAME="Pas d'ip de disponible"
+		Color="${RED}"
+	fi
+	echo "voici le style que je souhaite"
+	printf "\n\t=================================================\n"
+	printf "\t||\t\t      Détails du site: \t\t||\n"
+	printf "\t||\t   Adresse du site: "${YELLOW}""$1""${NC}"\t||\n"
+	printf "\t||\t   État du site: "$Color"$URL_EXIST"${NC}"\t||\n"
+	printf "\t||\t   Adresse IP du site: "$Color"$IP_HOSTNAME"${NC}"\t||\n"
+	printf "\t===================================================\n"
+	printf "\n"
+	echo "voici ce que cela donne, il manque les couleurs, le centrage et  une partie du cadre"
+	box_out "Détails du site:" "Adresse du site: "${YELLOW}""$1""${NC}"" "État du site: "$Color"$URL_EXIST"${NC}"" "Adresse IP du site: "$Color"$IP_HOSTNAME"${NC}""
+
+	box_out "ceci" "est un exemple" "sur plusieurs lignes" "mais ce n'est pas" "centré correctement" "il manque des bords"
+fi
